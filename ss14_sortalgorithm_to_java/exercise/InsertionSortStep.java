3b6de3c5100b0f52a3b8976d40d30af80ac4b0f9
tranvinhan <tranvinhan1106@gmail.com>
2023-03-06 18:50:55 +0700

    add .

--- ss14_sortalgorithm_to_java/exercise/InsertionSortStep.java
+++ ss14_sortalgorithm_to_java/exercise/InsertionSortStep.java
@@ -2,7 +2,7 @@ package ss14_sortalgorithm_to_java.exercise;
 
 import java.util.Scanner;
 
-public class InsertionSort {
+public class InsertionSortStep {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter list size:");
@@ -18,16 +18,19 @@ public class InsertionSort {
         }
         System.out.println("");
         insertionSort(list);
-    }
+    }//Nhập vào kích thước mảng và các phần tử của mảng
+
     public static void insertionSort(int[] list) {
         int pos, x;
-        for (int i = 1; i < list.length; i++) {
+        for (int i = 1; i < list.length; i++) {//đoạn array[0] đã sắp xếp
             x = list[i];
             pos = i;
             while (pos > 0 && x < list[pos - 1]) {
-                list[pos] = list[pos - 1];
+                System.out.println("Tráo đổi " + list[pos] + " với " + list[pos - 1]);
+                list[pos] = list[pos - 1]; // đổi chỗ
                 pos--;
             }
+
             list[pos] = x;
         }
         System.out.println("");
@@ -37,4 +40,5 @@ public class InsertionSort {
         }
         System.out.println();
     }
+
 }
